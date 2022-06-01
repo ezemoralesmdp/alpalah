9a8deb8cb237dfd35158dcac72b4c98079ae1e7e
tobiasgonzalez398 <tobygon988@gmail.com>
2022-05-31 22:32:13 -0300

    definimos estilos a botones, titulos, añadimos efecto al chevron con js y se agrego titulos a developers y work whit us

---
+++ js/chevron.js
@@ -0,0 +1,23 @@
+//scroll up 
+
+document.getElementById("chevron").addEventListener("click", scrollUp => {
+    let currentScroll = document.documentElement.scrollTop || document.body.scrollTop;
+    if (currentScroll > 0){
+        window.scrollTo (0, 0);
+    }
+});
+
+///
+
+chevron = document.getElementById("chevron");
+
+window.onscroll = function(){
+
+    let scroll = document.documentElement.scrollTop;
+    if(scroll > 500){
+        chevron.style.transform = "scale(1)";
+    }else if(scroll < 500){
+        chevron.style.transform = "scale(0)";
+    }
+
+}
